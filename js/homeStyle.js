b4dae1fde2070e8ed8276ddb70311a787d9b15b9
ZahraAbdolalian <abdolalianzahra@gamil.com>
2023-08-20 12:52:32 +0330

    add header bottom section (not complete)

---
+++ js/homeStyle.js
@@ -0,0 +1,38 @@
+// let $ = document
+
+// let portfolio_item = $.querySelector("#portfolio_item")
+// let add_item = $.querySelector("#add_item")
+
+// let portfolioArray = [
+//     'Portfolio Grid 2',
+//     'Portfolio Grid 3',
+//     'Portfolio Grid 4',
+//     'Masonry-Grid-2',
+//     'Masonry-Grid-3',
+//     'Masonry-Grid-4',
+//     'Masonry-Full-Width'
+// ]
+
+// let addArray = [
+//     'Cart Right',
+//     'Cart Left',
+//     'Cart Top',
+//     'Cart Bottom',
+//     'Cart-Model-Popup'
+// ]
+
+// portfolioArray.forEach(item => {
+//     portfolio_item.insertAdjacentHTML('beforeend',
+//     `<li class="list_hov_item submenu_item_hov">
+//         <a href="#" class="list_hov_link">${item}</a>
+//     </li>`
+//     )
+// })
+
+// addArray.forEach(item => {
+//     add_item.insertAdjacentHTML('beforeend',
+//     `<li class="list_hov_item submenu_item_hov">
+//         <a href="#" class="list_hov_link">${item}</a>
+//     </li>`
+//     )
+// })
