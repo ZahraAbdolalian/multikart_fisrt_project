bc0e9c5880f2374da99ddfbbb99a28038b8181e3
ZahraAbdolalian <abdolalianzahra@gamil.com>
2023-08-22 11:16:27 +0330

    The second part of trending product

--- js/homeStyle.js
+++ js/homeStyle.js
@@ -38,19 +38,23 @@ let $ = document
 // })
 
 
-// trending
+// trending part1
 
 let trendingContainer = $.querySelector('.trending_container')
+let trendingColorList
+let colorsArray
+let trendingImgBox
+
 
 let trendingArray = [
-    { src: 'images/trending1.jpg', name: 'Watch 1', discountedPrice: '$156.00', price: '$260.00', colors: 'silver' },
-    { src: 'images/trending2.jpg', name: 'Watch 2', discountedPrice: '$207.00', price: '$345.00', colors: 'silver' },
-    { src: 'images/trending3.jpg', name: 'Watch 3', discountedPrice: '$356.00', price: '$445.00', colors: 'silver' },
-    { src: 'images/trending4.jpg', name: 'Watch 4', discountedPrice: '$466.40', price: '$530.00', colors: 'silver' },
-    { src: 'images/trending5.jpg', name: 'Watch 5', discountedPrice: '$218.40', price: '$420.00', colors: 'silver' },
-    { src: 'images/trending6.jpg', name: 'Watch 6', discountedPrice: '$135.00', price: '$225.00', colors: 'silver' },
-    { src: 'images/trending7.jpg', name: 'Watch 7', discountedPrice: '$122.20', price: '$130.00', colors: 'silver' },
-    { src: 'images/trending4.jpg', name: 'Watch 8', discountedPrice: '$80.00', price: '$160.00', colors: 'silver' },
+    { id: 1, src: 'images/trending1.jpg', name: 'Watch 1', discountedPrice: '$156.00', price: '$260.00', colors: 'silver', onsale: false },
+    { id: 2, src: 'images/trending2.jpg', name: 'Watch 2', discountedPrice: '$207.00', price: '$345.00', colors: 'silver sienna', onsale: false },
+    { id: 3, src: 'images/trending3.jpg', name: 'Watch 3', discountedPrice: '$356.00', price: '$445.00', colors: 'navy', onsale: true },
+    { id: 4, src: 'images/trending4.jpg', name: 'Watch 4', discountedPrice: '$466.40', price: '$530.00', colors: 'bisque', onsale: true },
+    { id: 5, src: 'images/trending5.jpg', name: 'Watch 5', discountedPrice: '$218.40', price: '$420.00', colors: 'chocolate navy', onsale: true },
+    { id: 6, src: 'images/trending6.jpg', name: 'Watch 6', discountedPrice: '$135.00', price: '$225.00', colors: 'steelblue bisque', onsale: false },
+    { id: 7, src: 'images/trending7.jpg', name: 'Watch 7', discountedPrice: '$122.20', price: '$130.00', colors: 'silver wheat', onsale: false },
+    { id: 8, src: 'images/trending4.jpg', name: 'Watch 8', discountedPrice: '$80.00', price: '$160.00', colors: 'bisque palegoldenrod silver', onsale: false },
 ]
 
 trendingArray.forEach(product => {
@@ -61,7 +65,7 @@ trendingArray.forEach(product => {
                 <div class="trending_img_box">
                      <span class="trending_new">
                       new
-                    </span>
+                     </span>
                     <a href="">
                     <img class="trending_img" src=${product.src} alt="">
                     </a>
@@ -86,6 +90,90 @@ trendingArray.forEach(product => {
             <div class="trending_detail">
                 <div class="stars">
                     <div class="star">★</div>
+                    <div class="star">★</div>
+                    <div class="star">★</div>
+                    <div class="star">★</div>
+                    <div class="star">★</div>
+                </div>
+                <a href="">
+                    <h6 class="trending_name">${product.name}</h6>
+                </a>
+                <h4 class="trending_discounted_price"> ${product.discountedPrice}
+                    <del class="trending_price">${product.price}</del>
+                </h4>
+
+                <ul class="trending_color_list">
+
+                </ul>
+                
+            </div>
+    
+        </div>
+    </div>`
+    )
+    trendingColorList = $.querySelectorAll('.trending_color_list')
+    colorsArray = product.colors.split(' ')
+    trendingImgBox = $.querySelectorAll('.trending_img_box')
+    colorsArray.forEach(color => {
+        trendingColorList[product.id - 1].insertAdjacentHTML('beforeend',
+            `<li class="${color} trending_color"></li>`
+        )
+    })
+
+    if (product.onsale) {
+        trendingImgBox[product.id - 1].insertAdjacentHTML('afterbegin',
+        `<span class="trending_on_sale">
+            on sale
+        </span>`
+        )
+    }
+
+})
+
+// trending part2
+
+let trendingContainer2 = $.querySelector('.trending_container_part2')
+
+let trendingArray2 = [
+    { id: 1, src: 'images/trending1.jpg', name: 'Watch 1', discountedPrice: '$156.00', price: '$260.00', colors: 'silver', onsale: false},
+    { id: 2, src: 'images/trending2.jpg', name: 'Watch 2', discountedPrice: '$207.00', price: '$345.00', colors: 'silver sienna', onsale: false},
+    { id: 3, src: 'images/trending3.jpg', name: 'Watch 3', discountedPrice: '$356.00', price: '$445.00', colors: 'navy', onsale: true},
+    { id: 4, src: 'images/trending4.jpg', name: 'Watch 4', discountedPrice: '$466.40', price: '$530.00', colors: 'bisque', onsale: true},
+]
+
+trendingArray2.forEach(product => {
+
+    trendingContainer2.insertAdjacentHTML('beforeend',
+        ` <div class="trending_product_part2">
+        <div class="trending_product_box">
+            <div class="trending_img_box_part2">
+                <span class="trending_new">
+                    new
+                </span>
+                <a href="">
+                    <img class="trending_img" src="${product.src}" alt="">
+                </a>
+                <div class="trending_icons_part2">
+                    <a class="trending_icons_link" href="#">
+                        <i class="fa fa-shopping-cart fa-flip-horizontal shopping shopping_cart"></i>
+                    </a>
+
+                    <a class="trending_icons_link" href="#">
+                        <i class="fa fa-heart shopping heart"></i>
+                    </a>
+
+                    <a class="trending_icons_link" href="#">
+                        <i class="fa-solid fa-magnifying-glass shopping magnifying"></i>
+                    </a>
+
+                    <a class="trending_icons_link" href="#">
+                        <i class="fa fa-refresh shopping refresh"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="trending_detail">
+                <div class="stars stars_part2">
+                    <div class="star">★</div>
                     <div class="star">★</i></div>
                     <div class="star">★</i></div>
                     <div class="star">★</i></div>
@@ -97,15 +185,30 @@ trendingArray.forEach(product => {
                 <h4 class="trending_discounted_price"> ${product.discountedPrice}
                     <del class="trending_price">${product.price}</del>
                 </h4>
-                <ul class="trending_color_list">
-                    <li class="${product.colors} trending_color"></li>
+                <ul class="trending_color_list_part2">
+                    
                 </ul>
             </div>
-    
+
         </div>
     </div>`
     )
-    console.log(product.colors);
+    trendingColorList = $.querySelectorAll('.trending_color_list_part2')
+    colorsArray = product.colors.split(' ')
+    trendingImgBox = $.querySelectorAll('.trending_img_box_part2')
+    console.log(trendingImgBox);
+    colorsArray.forEach(color => {
+        trendingColorList[product.id - 1].insertAdjacentHTML('beforeend',
+            `<li class="${color} trending_color"></li>`
+        )
+    })
+
+    if (product.onsale) {
+        trendingImgBox[product.id - 1].insertAdjacentHTML('afterbegin',
+        `<span class="trending_on_sale">
+            on sale
+        </span>`
+        )
+    }
+
 })
-
-
