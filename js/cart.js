d1e9fa246ddeab1d658ed0b41ef0d77a28e311e4
ZahraAbdolalian <abdolalianzahra@gamil.com>
2023-08-31 22:39:56 +0330

    add some details to home page

--- js/cart.js
+++ js/cart.js
@@ -21,6 +21,8 @@ let quantityNumber = $.querySelector('.quantity_num')
 let quantityBtnLeft = $.querySelector('.quantity_btn_left')
 let quantityBtnRight = $.querySelector('.quantity_btn_right')
 let quantityLinkBtn = $.querySelectorAll('.quantity_link_btn')
+let alretContainer = $.querySelector('.alret_container')
+let alertMark = $.querySelector('.alert_mark')
 
 
 quantityBtnLeft.addEventListener('click', () => {
@@ -31,23 +33,57 @@ quantityBtnLeft.addEventListener('click', () => {
         quantityNumber.innerHTML = num
     }
 
-    if (num <= 15) {
-        quantityLinkBtn.forEach(button => {
-            button.classList.replace('disable', 'able')
-            button.href = ''
-        })
-    }
 })
 
 quantityBtnRight.addEventListener('click', () => {
     let num = quantityNumber.innerHTML
-    num++
-    quantityNumber.innerHTML = num
+    if (num < 15) {
+
+        num++
+        quantityNumber.innerHTML = num
+    }
 
     if (num > 15) {
-        quantityLinkBtn.forEach(button => {
-            button.classList.replace('able', 'disable')
-            button.href = ' URL:void(0)'
-        })
+        alretContainer.classList.remove('disable')
+        setTimeout(() => {
+            alretContainer.classList.add('disable')
+        }, 2000)
     }
 })
+
+alertMark.addEventListener('click', () => {
+    alretContainer.classList.add('disable')
+})
+
+
+// total product price
+let productTotalPrice = $.querySelector('.product_total_price')
+let quantityNum = $.querySelector('.quantity_num')
+let productPrice = $.querySelector('.product_price').innerHTML.slice(2)
+
+function setTotalProductPrice() {
+    productTotalPrice.innerHTML = `$${(quantityNum.innerHTML * productPrice).toFixed(2)}`
+}
+
+setTotalProductPrice()
+
+quantityBtnLeft.addEventListener('click', setTotalProductPrice)
+quantityBtnRight.addEventListener('click', setTotalProductPrice)
+
+// total price
+
+let allProductPrice = $.querySelectorAll('.product_total_price')
+let totalPrice = $.querySelector('#price_amount')
+let result = 0
+
+function setTotalPrice() {
+    result = 0
+    allProductPrice.forEach(price => {
+        result += +price.innerHTML.slice(1)
+    })
+    totalPrice.innerHTML = `$${result.toFixed(2)}`
+}
+setTotalPrice()
+
+quantityBtnLeft.addEventListener('click', setTotalPrice)
+quantityBtnRight.addEventListener('click', setTotalPrice)
--- js/home.js
+++ js/home.js
@@ -88,7 +88,7 @@ function sliderHandler() {
             `
         <h4 class="slider_subtitile">${sliderArray[1].subtitile}</h4>
         <h1 class="slider_title">${sliderArray[1].title}</h1>
-        <a href="#" class="slider_btn">Shop now</a>
+        <a href="collection.html" class="slider_btn">Shop now</a>
         `
         )
         slider.classList.replace('slider_bgi1', 'slider_bgi2')
@@ -99,7 +99,7 @@ function sliderHandler() {
             ` 
         <h4 class="slider_subtitile">${sliderArray[0].subtitile}</h4>
         <h1 class="slider_title">${sliderArray[0].title}</h1>
-        <a href="#" class="slider_btn">Shop now</a>
+        <a href="collection.html" class="slider_btn">Shop now</a>
         `
         )
         slider.classList.replace('slider_bgi2', 'slider_bgi1')
@@ -114,75 +114,101 @@ arrowRight.addEventListener('click', sliderHandler)
 // trending part1
 
 let trendingContainer = $.querySelector('.trending_container')
+let trendingListSelector = $.querySelectorAll('.trending_list_link ')
 let trendingColorList
 let colorsArray
 let trendingImgBox
+let trendingSelector = trendingListSelector[0]
 
 
-let trendingArray = [
+
+
+
+let newArrivalArray = [
     { id: 1, src: 'images/trending1.jpg', name: 'Watch 1', discountedPrice: '$156.00', price: '$260.00', colors: 'silver', onsale: false },
     { id: 2, src: 'images/trending2.jpg', name: 'Watch 2', discountedPrice: '$207.00', price: '$345.00', colors: 'silver sienna', onsale: false },
     { id: 3, src: 'images/trending3.jpg', name: 'Watch 3', discountedPrice: '$356.00', price: '$445.00', colors: 'navy', onsale: true },
     { id: 4, src: 'images/trending4.jpg', name: 'Watch 4', discountedPrice: '$466.40', price: '$530.00', colors: 'bisque', onsale: true },
     { id: 5, src: 'images/trending5.jpg', name: 'Watch 5', discountedPrice: '$218.40', price: '$420.00', colors: 'chocolate navy', onsale: true },
     { id: 6, src: 'images/trending6.jpg', name: 'Watch 6', discountedPrice: '$135.00', price: '$225.00', colors: 'steelblue bisque', onsale: false },
-    { id: 7, src: 'images/trending7.jpg', name: 'Watch 7', discountedPrice: '$122.20', price: '$130.00', colors: 'silver wheat', onsale: false },
-    { id: 8, src: 'images/trending4.jpg', name: 'Watch 8', discountedPrice: '$80.00', price: '$160.00', colors: 'bisque palegoldenrod silver', onsale: false },
+    { id: 7, src: 'images/trending7.jpg', name: 'Watch 14', discountedPrice: '$122.20', price: '$130.00', colors: 'silver wheat', onsale: false },
+    { id: 8, src: 'images/trending4.jpg', name: 'Watch 16', discountedPrice: '$80.00', price: '$160.00', colors: 'bisque palegoldenrod silver', onsale: false },
 ]
 
-trendingArray.forEach(product => {
+let onSaleArray = [
+    { id: 1, src: 'images/trending5.jpg', name: 'Watch 5', discountedPrice: '$218.40', price: '$420.00', colors: 'chocolate navy', onsale: true },
+    { id: 2, src: 'images/watch14.jpg', name: 'Watch 6', discountedPrice: '$135.00', price: '$225.00', colors: 'steelblue bisque', onsale: false },
+    { id: 3, src: 'images/watch8.jpg', name: 'Watch 8', discountedPrice: '$80.00', price: '$160.00', colors: 'dimgrey sienna', onsale: false },
+    { id: 4, src: 'images/watch10.jpg', name: 'Watch 10', discountedPrice: '$346.50 ', price: '$495.00', colors: 'silver palegoldenrod', onsale: true },
+    { id: 5, src: 'images/watch13.jpg', name: 'Watch 13', discountedPrice: '$144.00 ', price: '$360.00', colors: 'black chocolate', onsale: true },
+    { id: 6, src: 'images/watch15.jpg', name: 'Watch 14', discountedPrice: '$122.20', price: '$130.00', colors: 'silver wheat', onsale: false },
+    { id: 7, src: 'images/trending2.jpg', name: 'Watch 15', discountedPrice: '$239.04 ', price: '$249.00', colors: 'sienna navy', onsale: true },
+    { id: 8, src: 'images/trending4.jpg', name: 'Watch 16', discountedPrice: '$80.00 ', price: '$160.00', colors: 'bisque palegoldenrod silver', onsale: false },
+]
 
+let bestSellersArray = [
+    { id: 1, src: 'images/watch7.jpg', name: 'Watch 7', discountedPrice: '$156.60 ', price: '$174.00', colors: 'wheat silver', onsale: false },
+    { id: 2, src: 'images/watch8.jpg', name: 'Watch 8', discountedPrice: '$132.30 ', price: '$189.00', colors: 'dimgrey sienna', onsale: false },
+    { id: 3, src: 'images/watch9.jpg', name: 'Watch 8', discountedPrice: '$80.00', price: '$160.00', colors: 'black sienna', onsale: false },
+    { id: 4, src: 'images/watch10.jpg', name: 'Watch 10', discountedPrice: '$346.50 ', price: '$495.00', colors: 'silver palegoldenrod', onsale: true },
+    { id: 5, src: 'images/watch11.jpg', name: 'Watch 11', discountedPrice: '$86.00 ', price: '$215.00', colors: 'dimgray chocolate', onsale: false },
+    { id: 6, src: 'images/watch12.jpg', name: 'Watch 12', discountedPrice: '$135.00', price: '$150.00', colors: 'palegoldenrod bisque', onsale: true },
+    { id: 7, src: 'images/watch13.jpg', name: 'Watch 13', discountedPrice: '$144.00 ', price: '$360.00', colors: 'black chocolate', onsale: true },
+    { id: 8, src: 'images/watch14.jpg', name: 'Watch 14', discountedPrice: '$265.05', price: '$285.00', colors: 'steelblue black', onsale: true },
+]
+
+function addProductToContainer(product) {
     trendingContainer.insertAdjacentHTML('beforeend',
         ` <div class="trending_product">
-            <div class="trending_product_box">
-                <div class="trending_img_box">
-                     <span class="trending_new">
-                      new
-                     </span>
-                    <a href="">
-                    <img class="trending_img" src=${product.src} alt="">
-                    </a>
-                    <div class="trending_icons">
-                        <a class="trending_icons_link" href="#">
-                            <i class="fa fa-shopping-cart fa-flip-horizontal shopping shopping_cart"></i>
-                        </a>
-    
-                        <a class="trending_icons_link" href="#">
-                         <i class="fa fa-heart shopping heart"></i>
-                        </a>
-    
-                        <a class="trending_icons_link" href="#">
-                            <i class="fa-solid fa-magnifying-glass shopping magnifying"></i>
-                        </a>
-    
-                        <a class="trending_icons_link" href="#">
-                            <i class="fa fa-refresh shopping refresh"></i>
-                        </a>
-                    </div>
-                </div>
-            <div class="trending_detail">
-                <div class="stars">
-                    <div class="star">★</div>
-                    <div class="star">★</div>
-                    <div class="star">★</div>
-                    <div class="star">★</div>
-                    <div class="star">★</div>
-                </div>
+        <div class="trending_product_box">
+            <div class="trending_img_box">
+                 <span class="trending_new">
+                  new
+                 </span>
                 <a href="">
-                    <h6 class="trending_name">${product.name}</h6>
+                <img class="trending_img" src=${product.src} alt="">
                 </a>
-                <h4 class="trending_discounted_price"> ${product.discountedPrice}
-                    <del class="trending_price">${product.price}</del>
-                </h4>
+                <div class="trending_icons">
+                    <a class="trending_icons_link" href="#">
+                        <i class="fa fa-shopping-cart fa-flip-horizontal shopping shopping_cart"></i>
+                    </a>
 
-                <ul class="trending_color_list">
+                    <a class="trending_icons_link" href="#">
+                     <i class="fa fa-heart shopping heart"></i>
+                    </a>
 
-                </ul>
-                
+                    <a class="trending_icons_link" href="#">
+                        <i class="fa-solid fa-magnifying-glass shopping magnifying"></i>
+                    </a>
+
+                    <a class="trending_icons_link" href="#">
+                        <i class="fa fa-refresh shopping refresh"></i>
+                    </a>
+                </div>
             </div>
-    
+        <div class="trending_detail">
+            <div class="stars">
+                <div class="star">★</div>
+                <div class="star">★</div>
+                <div class="star">★</div>
+                <div class="star">★</div>
+                <div class="star">★</div>
+            </div>
+            <a href="">
+                <h6 class="trending_name">${product.name}</h6>
+            </a>
+            <h4 class="trending_discounted_price"> ${product.discountedPrice}
+                <del class="trending_price">${product.price}</del>
+            </h4>
+
+            <ul class="trending_color_list">
+
+            </ul>
+            
         </div>
-    </div>`
+
+    </div>
+</div>`
     )
     trendingColorList = $.querySelectorAll('.trending_color_list')
     colorsArray = product.colors.split(' ')
@@ -196,12 +222,44 @@ trendingArray.forEach(product => {
     if (product.onsale) {
         trendingImgBox[product.id - 1].insertAdjacentHTML('afterbegin',
             `<span class="trending_on_sale">
-            on sale
-        </span>`
+        on sale
+    </span>`
         )
     }
+}
 
+newArrivalArray.forEach(product => {
+    addProductToContainer(product)
 })
+
+trendingListSelector.forEach(selector => {
+
+    selector.addEventListener('click', () => {
+        trendingContainer.innerHTML = ''
+        console.log(selector);
+        trendingSelector.classList.remove('selected')
+        selector.classList.add('selected')
+        trendingSelector = selector
+
+        if(selector.innerHTML == 'new arrival'){
+            console.log('yes');
+            newArrivalArray.forEach(product => {
+                addProductToContainer(product)
+            })
+        }else if(selector.innerHTML == 'on sale'){
+
+            onSaleArray.forEach(product => {
+                addProductToContainer(product)
+            })
+        }else{
+            bestSellersArray.forEach(product => {
+                addProductToContainer(product)
+            })
+        }
+    })
+})
+
+
 
 // trending part2
 
