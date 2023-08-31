cba275ded1ce6c0dec6574fc798267c5db5ce687
ZahraAbdolalian <abdolalianzahra@gamil.com>
2023-08-31 18:48:15 +0330

    add cart section with a product

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,53 @@
 let $ = document
+
+//header sticky
+
+let headerBottom = $.querySelector('.header_bottom')
+
+document.addEventListener('scroll', () => {
+
+    if (document.documentElement.scrollTop > 200) {
+        headerBottom.classList.add('sticky')
+    } else {
+        headerBottom.classList.remove('sticky')
+    }
+})
+
+
+
+// quantity button
+
+let quantityNumber = $.querySelector('.quantity_num')
+let quantityBtnLeft = $.querySelector('.quantity_btn_left')
+let quantityBtnRight = $.querySelector('.quantity_btn_right')
+let quantityLinkBtn = $.querySelectorAll('.quantity_link_btn')
+
+
+quantityBtnLeft.addEventListener('click', () => {
+    let num = quantityNumber.innerHTML
+
+    if (num > 1) {
+        num--
+        quantityNumber.innerHTML = num
+    }
+
+    if (num <= 15) {
+        quantityLinkBtn.forEach(button => {
+            button.classList.replace('disable', 'able')
+            button.href = ''
+        })
+    }
+})
+
+quantityBtnRight.addEventListener('click', () => {
+    let num = quantityNumber.innerHTML
+    num++
+    quantityNumber.innerHTML = num
+
+    if (num > 15) {
+        quantityLinkBtn.forEach(button => {
+            button.classList.replace('able', 'disable')
+            button.href = ' URL:void(0)'
+        })
+    }
+})
